8248ae8e9fe99146fa7f14a041b81e6df32c59aa
Jools <julian.smith@flexera.com>
2024-09-06 20:26:45 +0100

    first commit

---
+++ SDM/SDM/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program {
+  private static void Main(string[] args) {
+    Console.WriteLine("Hello, World!");
+    try {
+
+    }
+    catch(Exception e) {
+
+    }
+    finally {
+      Console.WriteLine("bye...");
+    }
+  }
+}
